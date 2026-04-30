9807d1664a1e75da28644487b2b547e3fb9588fc
SulphideZombie <142401003@smail.iitpkd.ac.in>
2026-04-30 17:36:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -266,10 +266,10 @@ def update_status(req: UpdateStatusRequest, db: Session = Depends(get_db)):
 
     return {"status": "success", "message": "State updated"}
 
-# import os
-# import uvicorn
+import os
+import uvicorn
 
-# port = int(os.environ.get("PORT", 8000))
+port = int(os.environ.get("PORT", 8000))
 
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
